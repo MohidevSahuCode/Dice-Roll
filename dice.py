ac364c7d87091045d787c3b1a0944ca70157e13f
M.S_Code <119150719+MohidevSahuCode@users.noreply.github.com>
2023-07-07 20:09:27 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,33 @@
+import random
+
+response = "y"
+
+while response == "y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("- - -")
+        print("- * -")
+        print("- - -")
+    elif no == 2:
+        print("- * -")
+        print("- - -")
+        print("- * -")
+    elif no == 3:
+        print("- - -")
+        print("* * *")
+        print("- - -")
+    elif no == 4:
+        print("* - -")
+        print("* * *")
+        print("- - -")
+    elif no == 5:
+        print("* - *")
+        print("- * -")
+        print("* - *")
+    elif no == 6:
+        print("* * *")
+        print("- - -")
+        print("* * *")
+        
+    response = input("Do you want to roll a dice . if yes then press 'y'. : ")
+
